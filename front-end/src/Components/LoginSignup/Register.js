5366710dcc3322a6f65587f52a675bfff9aee80a
Kajzelek <cygnar.patryk@gmail.com>
2024-10-30 13:47:05 +0100

    Frontend-basic-Registration-Module-Implemented

--- front-end/src/Components/LoginSignup/Register.js
+++ front-end/src/Components/LoginSignup/Register.js
@@ -1,29 +1,37 @@
 import { useRef, useState, useEffect } from "react";
 import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import axios from '../../api/axios';
 
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const REGISTER_URL = '/register';
 
 const Register = () => {
 
-    const userRef = useRef(null);
+    const userRef = useRef();
+    const emailRef = useRef();
     const errRef = useRef();
 
-    const [user, setUser] = useState("");
+    const [user, setUser] = useState('');
     const [validName, setValidName] = useState(false);
     const [userFocus, setUserFocus] = useState(false);
 
-    const [pwd, setPwd] = useState("");
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
     const [validPwd, setValidPwd] = useState(false);
     const [pwdFocus, setPwdFocus] = useState(false);
 
-    const [matchPwd, setMatchPwd] = useState("");
+    const [matchPwd, setMatchPwd] = useState('');
     const [validMatch, setValidMatchPwd] = useState(false);
     const [matchFocus, setMatchFocus] = useState(false);
 
-    const [errMsg, setErrMsg] = useState("");
+    const [errMsg, setErrMsg] = useState('');
     const [succeess, setSuccess] = useState(false);
 
     useEffect(() => {
@@ -38,6 +46,13 @@ const Register = () => {
     }, [user]);
 
     useEffect(() => {
+        const result = EMAIL_REGEX.test(email);
+        console.log(result)
+        console.log(email)
+        setValidEmail(result);
+    }, [email]);
+
+    useEffect(() => {
         const result = PWD_REGEX.test(pwd);
         console.log(result);
         console.log(pwd);
@@ -50,18 +65,72 @@ const Register = () => {
         setErrMsg('');
     }, [user, pwd, matchPwd]);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // if button enabled with JS hack
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+        try {
+            // const response = await axios.post(REGISTER_URL ,
+            //     JSON.stringify({user, email,pwd}),
+            //     {headers: {
+            //         'Content-Type': 'application/json',
+            //         withCredentials: true
+            //     }}
+
+            const response = await axios.post(`${REGISTER_URL}?password=${pwd}&username=${user}&email=${email}`, null, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    withCredentials: true
+                }
+            }
+        );
+    
+        console.log(response.data);
+        console.log(response.accessToken);
+        console.log(JSON.stringify(response))
+        setSuccess(true);
+        // clear input fields
+        }catch (error) {
+            if(!errRef?.response) {
+                setErrMsg("Network Error");
+            } else if (errRef.response.status === 409) {
+                setErrMsg("Username already exists");
+            } else {
+                setErrMsg("Registration failed");
+            }
+
+            errRef.current.focus();
+        }
+    }
+
 
     return(
+        <> 
+        {succeess ? (
+            <section>
+                <h1>Success!</h1>
+                <p>
+                    <a href="#">Sign In</a>
+                </p>
+            </section>
+            ) : (
         <section>
 
-        <p ref={errMsg} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive"> {errMsg} </p>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive"> {errMsg} </p>
 
         <h1>Register</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
 
                 <label htmlFor="username">
                     Username:
+                    <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                    <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
                 </label>
 
                 <input
@@ -83,6 +152,32 @@ const Register = () => {
                     4 to 24 characters.<br />
                     Must begin with a letter.<br />
                     Letters, numbers, underscores, hyphens allowed.
+                </p>
+
+
+                <label htmlFor="email">
+                    Email:
+                    <FontAwesomeIcon icon={faCheck} className={validEmail ? "valid" : "hide"} />
+                    <FontAwesomeIcon icon={faTimes} className={validEmail || !email ? "hide" : "invalid"} />
+                </label>
+
+                <input
+                    type="text"
+                    id="email"
+                    ref={emailRef}
+                    autoComplete="off"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                    aria-invalid={validEmail ? "false" : "true"}
+                    aria-describedby="emailnote"
+                    onFocus={() => setEmailFocus(true)}
+                    onBlur={() => setEmailFocus(false)}
+                />
+
+                <p id="emailnote" className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    Enter the correct email address. Example: JohnSnow@Gmail.com
                 </p>
 
                 <label htmlFor="password">
@@ -137,7 +232,17 @@ const Register = () => {
 
             </form>
 
+            <p>
+                        Already registered?<br />
+                        <span className="line">
+                            {/*put router link here*/}
+                            <a href="#">Sign In</a>
+                        </span>
+                    </p>
+
         </section>
+        )}
+        </>
     )
 }
 
