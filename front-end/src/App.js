1a27c335ce8cf5d0e9464bc9d30f9df4f7dc9a7e
Patryk Cygnar <patryk.cygnar@gmail.com>
2024-11-01 21:27:16 +0100

    Login System implemented

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import Register from './Components/LoginSignup/Register';
+import Login from './Components/LoginSignup/Login';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
-      {/* <LoginSignup/> */}
+      {/* <Register/> */}
+      <Login/>
     </div>
   );
 }
