8a3a531dd0cf4da603cc86e2d151d7d1c70a873f
Patryk Cygnar <patryk.cygnar@student.pk.edu.pl>
2024-11-04 12:44:37 +0100

    protected-routes update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import Register from './Components/LoginSignup/Register';
 import Login from './Components/LoginSignup/Login';
+import Layout from './Components/Layout';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      {/* <Register/> */}
-      <Login/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        {/* Public routes */}
+        <Route path="login" element={<Login/>}></Route>
+        <Route path="register" element={<Register/>}></Route>
+        {/* <Route path="unauthorized" element={<Unauthorized/>}></Route> */}
+
+        {/* Protected routes */}
+        {/* <Route path="/" element={<Home/>}></Route>
+        <Route path="AdminHome" element={<AdminHome/>}></Route> */}
+
+
+        {/* Catch others */}
+        {/* <Route path="*" element={<Missing/>}></Route> */}
+
+      </Route>  
+    </Routes>
   );
 }
 
